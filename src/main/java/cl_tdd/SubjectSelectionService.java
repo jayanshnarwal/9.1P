a48796034578cc7ce84159521ca7070e195db713
Jayansh narwal <141374346+jayanshnarwal@users.noreply.github.com>
2025-05-29 23:39:51 +0530

    Update SubjectSelectionService.java

--- src/main/java/cl_tdd/SubjectSelectionService.java
+++ src/main/java/cl_tdd/SubjectSelectionService.java
@@ -4,14 +4,15 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class SubjectSelectionService {
-	private final Set<String> availableSubjects = new HashSet<>();
-	{
-	    availableSubjects.add("SIT333");
-	    availableSubjects.add("SIT330");
-	    availableSubjects.add("SIT310");
-	    availableSubjects.add("SIT764");
-	}
+    private final Set<String> availableSubjects = new HashSet<>();
     private final Set<String> selectedSubjects = new HashSet<>();
+
+    {
+        availableSubjects.add("SIT333");
+        availableSubjects.add("SIT330");
+        availableSubjects.add("SIT310");
+        availableSubjects.add("SIT764");
+    }
 
     public String selectSubject(String subjectCode) {
         if (!availableSubjects.contains(subjectCode)) {
